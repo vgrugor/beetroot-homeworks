085d16a534c3c41939fae74ea156a8fc488199e0
vgrugor <vgrugor@gmail.com>
2021-09-12 18:41:21 +0300

    Implemented the greeting in different languages

---
+++ 09_conditional_operators/php_conditional_operators.php
@@ -0,0 +1,25 @@
+<?php
+
+$users[3] = ["name" => "Vlad", "email" => "Vlad@test.com", 'lang' => 'en'];
+$users[18] = ["name" => "Igor", "email" => "Igor@test.com", 'lang' => 'ru'];
+$users[15] = ["name" => "Ivan", "email" => "Ivan@test.com", 'lang' => 'ua'];
+$users[9] = ["name" => "Petro", "email" => "Petro@test.com", 'lang' => 'fr'];
+$users[7] = ["name" => "Sergii", "email" => "Sergii@test.com", 'lang' => 'ru'];
+
+$lang['ru'] = 'Привет, пользователь!';
+$lang['en'] = 'Hello, user!';
+$lang['ua'] = 'Привіт, користувач!';
+$lang['fr'] = 'Bonjour, utilisateur';
+$lang['de'] = 'Hallo, Benutzer';
+
+krsort($users);
+
+$maxIdUser = reset($users);
+$minIdUser = end($users);
+
+if ($maxIdUser['lang'] === $minIdUser['lang']) {
+    echo $lang[$maxIdUser['lang']] . PHP_EOL;
+} else {
+    echo $lang[$maxIdUser['lang']] . PHP_EOL .
+        $lang[$minIdUser['lang']] . PHP_EOL;
+}
