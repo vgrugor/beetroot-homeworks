84ed071d5bbdb95121250312de9eefa629128905
vgrugor <vgrugor@gmail.com>
2021-09-12 18:46:34 +0300

    Comments added

--- 09_conditional_operators/php_conditional_operators.php
+++ 09_conditional_operators/php_conditional_operators.php
@@ -1,11 +1,13 @@
 <?php
 
+//масив користувачів
 $users[3] = ["name" => "Vlad", "email" => "Vlad@test.com", 'lang' => 'en'];
 $users[18] = ["name" => "Igor", "email" => "Igor@test.com", 'lang' => 'ru'];
 $users[15] = ["name" => "Ivan", "email" => "Ivan@test.com", 'lang' => 'ua'];
 $users[9] = ["name" => "Petro", "email" => "Petro@test.com", 'lang' => 'fr'];
 $users[7] = ["name" => "Sergii", "email" => "Sergii@test.com", 'lang' => 'ru'];
 
+//масив мов
 $lang['ru'] = 'Привет, пользователь!';
 $lang['en'] = 'Hello, user!';
 $lang['ua'] = 'Привіт, користувач!';
@@ -17,6 +19,7 @@ krsort($users);
 $maxIdUser = reset($users);
 $minIdUser = end($users);
 
+//виводимо привітання
 if ($maxIdUser['lang'] === $minIdUser['lang']) {
     echo $lang[$maxIdUser['lang']] . PHP_EOL;
 } else {
